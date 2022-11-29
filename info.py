3cf24a5e1b98586b1eede94657496925cff4552e
STThamizhan001 <117282214+STThamizhan001@users.noreply.github.com>
2022-11-29 07:04:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -38,7 +38,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001613135994'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'ST√Thamizhan')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'ST Thamizhan')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), False)
